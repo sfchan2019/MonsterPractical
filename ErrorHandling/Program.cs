e792628ad79959c094555e4d05b870ba393422db
Seng Fai Chan <SChan@spartaglobal.com>
2019-09-02 09:53:33 +0100

    Initialize objects

--- ErrorHandling/Program.cs
+++ ErrorHandling/Program.cs
@@ -10,30 +10,104 @@ namespace ErrorHandling
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            game.Initialize();
+            game.Do();
+        }
+    }
 
+    class Game
+    {
+        public void Initialize()
+        {
+            Monsters_Cohort monsterCohort = new Monsters_Cohort();
+            for (int i = 0; i < 5; i++)
+            {
+                Monster_Student ms = new Monster_Student(i);
+                monsterCohort.AddStudent(ms);
+            }
         }
 
-        class Monster_Student
+        public void Do()
         {
-            int id;
-            string eyes;
-            string hair;
-            float scaryLevel;
-            string specialAbility;
 
-            public void Jump() { }
+        }
+    }
 
-            public void Scream() { }
+    class Monster_Student
+    {
+        int id;
+        public int Id
+        {
+            get { return id; }
+        }
+        string eyes;
+        public string Eyes
+        {
+            get { return eyes; }
+            set { eyes = value; }
+        }
+        string hair;
+        public string Hair
+        {
+            get { return hair; }
+            set { hair = value; }
+        }
+        float scaryLevel;
+        public float ScaryLevel
+        {
+            get { return scaryLevel; }
+            set { scaryLevel = value; }
+        }
+        string specialAbility;
+        public string SpecialAbility
+        {
+            get { return specialAbility; }
+            set { specialAbility = value; }
         }
 
-        class Monsters_Cohort
+        public Monster_Student(int id)
         {
-            int subject;
-            string startDate;
-            List<Monster_Student> Students;
-            public void AddStudent(Monster_Student ms) { }
-            public void ListStudent() { }
-            public void ImportStudent() { }
+            this.id = id;
+        }
+        public Monster_Student(int id, string eyes, string hair, float scaryLevel, string specAbility)
+        {
+            this.id = id;
+            this.eyes = eyes;
+            this.hair = hair;
+            this.scaryLevel = scaryLevel;
+            specialAbility = specAbility;
+        }
+        public void Jump()
+        {
+            Console.WriteLine("Jump");
+        }
+
+        public void Scream()
+        {
+            Console.WriteLine("Scream");
+        }
+    }
+
+    class Monsters_Cohort
+    {
+        int subject;
+        string startDate;
+        List<Monster_Student> students = new List<Monster_Student>();
+
+        public void AddStudent(Monster_Student ms)
+        {
+            students.Add(ms);
+        }
+        public void ListStudent()
+        {
+            foreach (Monster_Student ms in students)
+            {
+                Console.WriteLine(ms.Id.ToString() + ms.SpecialAbility);
+            }
+        }
+        public void ImportStudent()
+        {
 
         }
     }
