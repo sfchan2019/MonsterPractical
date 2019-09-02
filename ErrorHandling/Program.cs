de55e60090e95fc37ba1579a72647f9bf0bcddf0
Seng Fai Chan <SChan@spartaglobal.com>
2019-09-02 09:33:59 +0100

    Monster_Student Class

---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        class Monster_Student
+        {
+            int id;
+            string eyes;
+            string hair;
+            float scaryLevel;
+            string specialAbility;
+
+            public void Jump() { }
+
+            public void Scream() { }
+        }
+    }
+}
